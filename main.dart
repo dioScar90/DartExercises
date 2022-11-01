d6e515e8ab3911e44973a03ad121c5e9d649102f
Diogo L Scarmagnani <diogols@live.com>
2022-11-01 17:12:12 -0300

    regexp

--- main.dart
+++ main.dart
@@ -1,49 +1,54 @@
-print('Hello, world!');
-
 import 'dart:io';
 import 'dart:math';
 
-int menu() {
-	print('\nMenu de cálculos');
-	print('1. Número ao quadrado');
-	print('2. Número ao cubo');
-	print('3. Raiz do número');
-	print('4. Raiz cúbica do número');
-	print('5. Sair');
-	print('Qual é a opção desejada? ');
-	int op = int.parse(stdin.readLineSync());
-	return op;
-}
-
-int calcularQuadrado(int num) {
-	return pow(num,2);
-}
- 
-int calcularCubo(int num) {
-	return pow(num,3);
+String ToUpperFirst(String name) {
+  name = name.trim();
+  String newStr = '';
+  String newName = name.replaceAll(RegExp(r'\s+'), ' ');
+  final arrStr = newName.split(' ');
+  final prepositions = ['di', 'da', 'das', 'do', 'dos', 'de', 'e', 'von', 'van', 'le', 'la', 'du', 'des', 'del', 'della', 'der'];
+  
+  for (final item in arrStr) {
+    if (prepositions.contains(item.toLowerCase())) {
+      newStr += item.toLowerCase();
+    }
+    else {
+      if (item.substring(0, 2).toUpperCase() == "O'") {
+        newStr += item.substring(0, 3).toUpperCase() + item.substring(3).toLowerCase();
+      }
+      else if (item.length > 3 && item.substring(0, 3).toLowerCase() == "mc" + RegExp("[a-z]")) {
+        newStr += item[0].toUpperCase() + item[1].toLowerCase() + item[2].toUpperCase() + item.substring(3).toLowerCase();
+      }
+      else if (item.length > 4 && item.substring(0, 4).toLowerCase() == "macc") {
+        newStr += item[0].toUpperCase() + item.substring(1, 3).toLowerCase() + item[3].toUpperCase() + item.substring(4).toLowerCase();
+      }
+      else {
+        newStr += item[0].toUpperCase() + item.substring(1).toLowerCase();
+      }
+    }
+    
+    newStr += arrStr.last == item ? '' : ' ';
+  }
+  
+  return newStr;
 }
 
 void main() {
-	int opcao = menu();
-	int numero;
-
-	if ([1,2,3,4].contains(opcao)) {
-		stdout.write('Informe um número: ');
-		numero = int.parse(stdin.readLineSync());
-	}
-
-	switch (opcao) {
-		case 1:
-			final quadrado = calcularQuadrado(numero);
-			print('O quadrado do número $numero é: $quadrado.');
-			break;
-		case 2:
-			final cubo = calcularCubo(numero);
-			print('O cubo do número $numero é: $cubo.');
-			break;
-		default:
-			exit(0);
-	}
-	
-	main();
+  final nomes = [
+    " vincent VAN gogh",
+    " diogo dE lIMA SCARMAGNANI",
+    " edwin     VAN DER SAR     ",
+    " luiz philippe de orleans e bragança",
+    " roBERt o'doNNElL ",
+    "affonso   deLLA    mÔnica    ",
+    "randall mccoy",
+    "charles de gaulle",
+    "   leonardo  di caprio"
+  ];
+  
+  int i = 0;
+  for (final nome in nomes) {
+    i++;
+    print ("Nome $i: ${ToUpperFirst(nome)}.");
+  }
 }
