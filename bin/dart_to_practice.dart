89d4d45c595393ad2379ea7010e0a502225dd78a
Diogo L Scarmagnani <diogols@live.com>
2022-10-31 22:24:18 -0300

    banda

--- bin/dart_to_practice.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_to_practice/dart_to_practice.dart' as dart_to_practice;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_to_practice.calculate()}!');
-}
--- test/DartToPractice.dart
+++ test/DartToPractice.dart
@@ -1,4 +1,4 @@
-import 'package:dart_to_practice/dart_to_practice.dart';
+import 'package:DartToPractice/DartToPractice.dart';
 import 'package:test/test.dart';
 
 void main() {
