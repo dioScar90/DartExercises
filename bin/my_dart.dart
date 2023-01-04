c359b202a00d6b01a35dbe19ad970e0fa97442ea
Diogo L Scarmagnani <diogols@live.com>
2023-01-04 16:17:27 -0300

    substituir regex por ||

--- bin/my_dart.dart
+++ bin/my_dart.dart
@@ -4,23 +4,25 @@ import 'dart:io';
 
 void main() {
 	final exemples = [
-	  '   ZÉ NETO   e CRIstiANO',
-	  ' vincént VAN gogh',
-	  ' diogo dE lIMA SCARMAGNANI',
-	  ' edwin     VAN DER SAR     ',
-	  ' luiz philippe de orleans e bragança',
-	  ' roBERt o\'doNNElL ',
-	  'affonso   deLLA    mÔnica    ',
-	  'randall mccoy',
-	  'charles de gaulle',
-	  '   leonardo  di caprio'
+		'   ZÉ NETO   e CRIstiANO',
+		' vincént VAN gogh',
+		' diogo dE lIMA SCARMAGNANI',
+		' edwin     VAN DER SAR     ',
+		' luiz philippe de orleans e bragança',
+		' roBERt o\'doNNElL ',
+		'affonso   deLLA    mÔnica    ',
+		'randall mccoy',
+		'   bRUCe     MCLAReN',
+		' MARY   MaCLAREN      ',
+		'charles de gaulle',
+		'   leonardo  di caprio'
 	];
   
 	print('\nOutros exemplos de nomes:');
 	int i = 0;
 	for (String exemple in exemples) {
-	  i++;
-	  print("Nome $i: ${editar.EditarNomes.toUpperFirst(exemple)}.");
+		i++;
+		print("Nome $i: ${editar.EditarNomes.toUpperFirst(exemple)}.");
 	}
 
 	// stdout.write('Escreva um nome: ');
--- lib/EditarNomes.dart
+++ lib/EditarNomes.dart
@@ -1,5 +1,5 @@
 class EditarNomes {
-	static String toUpperFirst(String? name) {
+  	static String toUpperFirst(String? name) {
 		if (name == null) {
 			return 'Nome inválido';
 		}
@@ -8,47 +8,25 @@ class EditarNomes {
 		String newStr = '';
 		String newName = name.replaceAll(RegExp(r'\s+'), ' ');
 		final arrStr = newName.split(' ');
-		final prepositions = [
-			'di',
-			'da',
-			'das',
-			'do',
-			'dos',
-			'de',
-			'e',
-			'von',
-			'van',
-			'le',
-			'la',
-			'du',
-			'des',
-			'del',
-			'della',
-			'der'
-		];
-		final regToCompare = RegExp(r'(mac|mc)([^aeiouAEIOU\W0-9]{1})');
-
+		final prepositions = [ 'di', 'da', 'das', 'do', 'dos', 'de', 'e', 'von', 'van', 'le', 'la', 'du', 'des', 'del', 'della', 'der' ];
+		final vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+		
 		for (final item in arrStr) {
 			if (prepositions.contains(item.toLowerCase())) {
 				newStr += item.toLowerCase();
-			} else {
+			}
+			else {
 				if (item.substring(0, 2).toUpperCase() == 'O\'') {
-					newStr += item.substring(0, 3).toUpperCase() +
-							item.substring(3).toLowerCase();
-				} else if (item.length > 3 &&
-						regToCompare.hasMatch(item.substring(0, 3).toLowerCase())) {
-					newStr += item[0].toUpperCase() +
-							item[1].toLowerCase() +
-							item[2].toUpperCase() +
-							item.substring(3).toLowerCase();
-				} else if (item.length > 4 &&
-						regToCompare.hasMatch(item.substring(0, 4).toLowerCase())) {
-					newStr += item[0].toUpperCase() +
-							item.substring(1, 3).toLowerCase() +
-							item[3].toUpperCase() +
-							item.substring(4).toLowerCase();
-				} else {
-					newStr += item[0].toUpperCase() + item.substring(1).toLowerCase();
+					newStr += item.substring(0, 3).toUpperCase() + item.substring(3).toLowerCase();
+				}
+				else if (item.length > 3 && item.toLowerCase().startsWith('mc') && vowels.contains(item[3].toLowerCase())) {
+					newStr += item[0].toUpperCase() + item[1].toLowerCase() + item[2].toUpperCase() + item.substring(3).toLowerCase();
+				}
+				else if (item.length > 4 && item.toLowerCase().startsWith('mac') && vowels.contains(item[4].toLowerCase())) {
+					newStr += item[0].toUpperCase() + item.substring(1, 3).toLowerCase() + item[3].toUpperCase() + item.substring(4).toLowerCase();
+				}
+				else {
+				newStr += item[0].toUpperCase() + item.substring(1).toLowerCase();
 				}
 			}
 
@@ -56,5 +34,5 @@ class EditarNomes {
 		}
 
 		return newStr;
-	}
+  	}
 }
